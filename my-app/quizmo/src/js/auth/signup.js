a4bbe6a7f7d898d59b66b8cf254e483a3ff18939
szage07 <cjbuquis@gmail.com>
2023-12-20 08:55:27 +0800

    1st step sa functionality

---
+++ my-app/quizmo/src/js/auth/signup.js
@@ -0,0 +1,10 @@
+import { supabase } from "../main";
+
+const signup_form = document.getElementById('signup_form');
+
+signup_form.onsubmit = async (e) => {
+    e.preventDefault();
+
+    alert("imong ge tap");
+
+};
