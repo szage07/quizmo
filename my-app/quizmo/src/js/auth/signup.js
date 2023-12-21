b586db9e001f3f111fde4549d229428d1d0a4ce1
szage07 <cjbuquis@gmail.com>
2023-12-21 11:55:52 +0800

    2nd vid pa sa functionality error

--- my-app/quizmo/src/js/auth/signup.js
+++ my-app/quizmo/src/js/auth/signup.js
@@ -1,4 +1,4 @@
-import { supabase } from "../main";
+
 
 const signup_form = document.getElementById('signup_form');
 
@@ -6,39 +6,39 @@ signup_form.onsubmit = async (e) => {
     e.preventDefault();
 
     const formData = new FormData(signup_form);
-    if( formData.get("password") == formData.get("confirm_password")){
-     
-     
+    if (formData.get("password") == formData.get("confirm_password")) {
+        const { data, error } = await supabase.auth.signUp({
+            email: formData.get("email"),
+            password: formData.get("password"),
+            
+        });
+    console.log(data);
+        if (error) {
+            console.error(error);
+            return;
+        }
 
-        
-const { data, error } = await supabase.auth.signUp({
-    email:formData.get("email"),
-    password:formData.get("password"),
-  })
+        if (user) {
+            const { data: insertData, error: insertError } = await supabase
+                .from('user_infos')
+                .insert([
+                    {
+                        firstname: formData.get("firstname"),
+                        lastname: formData.get("lastname"),
+                        birthdate: formData.get("birthdate"),
+                        user_id: user.id,
+                        
+                    },
+                ])
+                .select();
 
-    let user_id = data.user.id;
+            if (insertError) {
+                console.error(insertError);
+                return;
+            }
 
-    if(user_id != null){ // corrected here
-        const { data, error } = await supabase
-        .from('user_infos')
-        .insert([
-          { firstname: formData.get("firstname"), 
-            lastname: formData.get("lastname"),
-            birthdate: formData.get("birthdate"),
-            user_id: user_id,
-            birthdate: formData.get("mobile_number"),
-        },
-
-        ])
-        .select()
+            console.log(insertData);
+        }
+        signup_form.reset();
     }
-
-        
-
-   console.log(data);
-   console.log(error);
-
-
-    }
-
 };
