4213cac30241798e9a55bd469ab23230c76e540b
szage07 <cjbuquis@gmail.com>
2023-12-21 15:42:49 +0800

    2nd vid completed

--- my-app/quizmo/src/js/auth/signup.js
+++ my-app/quizmo/src/js/auth/signup.js
@@ -1,10 +1,50 @@
-import { supabase } from "../main";
+import { supabase, successNotification, errorNotification } from "../main";
 
 const signup_form = document.getElementById('signup_form');
 
 signup_form.onsubmit = async (e) => {
     e.preventDefault();
+   
+    document.querySelector("#signup_form button").disabled = true;
+    document.querySelector(
+         "#signup_form button"
+    ).innerHTML = '<div class="spinner-border me-2" role="status"> </div> <span>Loading...</span>';
+   
 
-    alert("imong ge tap");
+    const formData = new FormData(signup_form);
+ if (formData.get("password") == formData.get("confirm_password")){
+    alert("Passwords does match");
 
+    const { data, error } = await supabase.auth.signUp({
+        email: formData.get("email"),
+        password: formData.get("password"),
+      });
+
+     let user_id = data.user.id
+     if (user_id != null) {  
+        const { data, error } = await supabase
+        .from("user_infos")
+        .insert([
+       { 
+        firstname: formData.get("firstname"),
+        lastname: formData.get("lastname"),
+        birthdate: formData.get("birthdate"),
+        user_id: user_id,
+
+       },
+      ])
+   .select();
+        
+     
+    if (error == null) successNotification("Registered Successfully", 5);
+    else {
+        errorNotification("Something went wrong. Unable to register.", 5);
+        console.log(error);
+    }
+
+ signup_form.reset();
+ document.querySelector("#signup_form button").disabled = false;
+ document.querySelector("#signup_form button").innerHTML = 'Sign up';
+  }
+ }
 };
--- my-app/quizmo/src/js/main.js
+++ my-app/quizmo/src/js/main.js
@@ -9,4 +9,30 @@ import { createClient } from '@supabase/supabase-js'
 // Create a single supabase client for interacting with your database
 const supabase = createClient('https://xeajiibztieslaxpthsi.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhlYWppaWJ6dGllc2xheHB0aHNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA1NzIyOTMsImV4cCI6MjAxNjE0ODI5M30.LRNjtF1rSZZjXOvi5SKMWIZidvUSHpfZj_JZqrxK71c')
 
-export{supabase};
+function successNotification (message, seconds = 0){
+    document.querySelector(".alert-success").classList.remove("d-none");
+    document.querySelector(".alert-success").classList.add("d-block");
+    document.querySelector(".alert-success").innerHTML = message;
+
+    if (seconds != 0) {
+        setTimeout(function () {
+            document.querySelector(".alert-success").classList.remove("d-block");
+            document.querySelector(".alert-success").classList.add("d-none");      
+        }, seconds * 1000);
+    }
+}
+
+function errorNotification (message, seconds = 0){
+    document.querySelector(".alert-danger").classList.remove("d-none");
+    document.querySelector(".alert-danger").classList.add("d-block");
+    document.querySelector(".alert-danger").innerHTML = message;
+
+    if (seconds != 0) {
+        setTimeout(function () {
+            document.querySelector(".alert-danger").classList.remove("d-block");
+            document.querySelector(".alert-danger").classList.add("d-none");      
+        }, seconds * 1000);
+    }
+}
+
+export{supabase, successNotification, errorNotification};
