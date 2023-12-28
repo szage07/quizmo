15cb04576892ef03977448c6a87ecce7a2976e27
szage07 <cjbuquis@gmail.com>
2023-12-28 18:22:30 +0800

    new column quiz taker

--- my-app/quizmo/src/js/system/numericalability.js
+++ my-app/quizmo/src/js/system/numericalability.js
@@ -1,6 +1,21 @@
 import { supabase } from "../main";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+    const storedTokenData = localStorage.getItem('sb-xeajiibztieslaxpthsi-auth-token');
+
+    if (storedTokenData) {
+        const tokenData = JSON.parse(storedTokenData);
+        const userId = tokenData?.user?.id;
+
+        if (userId) {
+            localStorage.setItem('userId', userId); // Store the user ID in local storage
+        } else {
+            console.error('User ID not found in stored token data.');
+        }
+    } else {
+        console.error('No token data found in localStorage.');
+    }
+
     const radioButtons = document.querySelectorAll('input[type="radio"]');
     radioButtons.forEach(button => {
         button.addEventListener('change', handleUserResponse);
@@ -9,25 +24,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function handleUserResponse(event) {
     const questionNumber = event.target.closest('.question').getAttribute('data-question-number');
+    console.log('Question Number:', questionNumber);
     const selectedValue = event.target.value;
 
-    // Assuming you have a table named 'questions' in your Supabase database
-    supabase
-        .from('questions')
-        .update({
-            users_choice: selectedValue
-        })
-        .eq('question_id', questionNumber) // Update where 'question_id' matches the specific question number
-        .then(response => {
-            console.log('User response updated:', response);
+    const userId = localStorage.getItem('userId'); // Retrieve user ID from local storage
 
-            // Log the updated data
-            console.log('Updated Data:', {
-                question_id: questionNumber,
-                users_choice: selectedValue
+    if (userId) {
+        supabase
+            .from('questions')
+            .update({
+                users_choice: selectedValue,
+                quiz_taker: userId // Update the quiz_taker column with the user ID
+            })
+            .eq('question_id', questionNumber)
+            .then(response => {
+                console.log('User response updated:', response);
+
+                // Log the updated data
+                console.log('Updated Data:', {
+                    question_id: questionNumber,
+                    users_choice: selectedValue,
+                    quiz_taker: userId
+                });
+            })
+            .catch(error => {
+                console.error('Error updating user response:', error);
             });
-        })
-        .catch(error => {
-            console.error('Error updating user response:', error);
-        });
+    } else {
+        console.error('User ID not available.');
+    }
 }
